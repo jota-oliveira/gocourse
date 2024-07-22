0e3c8c29fedb9efeb1b40df0242b24f4936d4b1c
João Oliveira <joao.aoliveira@mercadolivre.com>
2024-07-22 09:20:42 -0300

    Adding base template and cache control

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -7,9 +7,9 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplateTest(w, "home")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	render.RenderTemplateTest(w, "about")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,17 +1,65 @@
 package render
 
 import (
-	"net/http"
+	"fmt"
 	"html/template"
 	"log"
+	"net/http"
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 
 	if err != nil {
-			log.Fatal("Error parsing template: ", err)
+			fmt.Println("Error parsing template: ", err)
 			return
 	}
+}
+
+var templateCache = make(map[string]*template.Template)
+
+func RenderTemplateTest(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// check if we already have the template in the cache
+	// it will return the value and a boolean, we care about the boolean now
+	_, inMap := templateCache[t]
+
+	if !inMap {
+		log.Println("Creating template cache for", t)
+		err = createTemplateCache(t)
+
+		if err != nil {
+			log.Println("Error creating template cache", err)
+		}
+	} else {
+		log.Println("Using cached template")
+	}
+
+	tmpl = templateCache[t]
+
+	err = tmpl.Execute(w, nil)
+	
+	if err != nil {
+		log.Println("Error creating template cache", err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s.page.tmpl", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	tmpl, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		return err
+	}
+
+	templateCache[t] = tmpl
+
+	return nil
 }
